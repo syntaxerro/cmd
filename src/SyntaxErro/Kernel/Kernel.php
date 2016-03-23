93981736d40e231564638f9a84987c1a6ec0d60a
marcinlacina <marcin.lacina@zslt.klucznet.pl>
2016-03-23 19:41:53 +0100

    remove commands.yml: auto register commands from SyntaxErro\Command

--- src/SyntaxErro/Kernel/Kernel.php
+++ src/SyntaxErro/Kernel/Kernel.php
@@ -8,11 +8,9 @@ use SyntaxErro\Exception\InvalidCommandException;
 use SyntaxErro\Exception\InvalidConfigException;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Application;
-use Symfony\Component\Yaml\Parser;
 /**
  * Class Kernel
- * Contain Application from Symfony components and configPath constant with path to config file.
- * Config file should be in $ProjectRoot/src/SyntaxErro/Resources/config/commands.yml
+ * Contain Application from Symfony components. Register commands from SyntaxErro\Command
  * @package SyntaxErro\Kernel
  */
 class Kernel
@@ -23,20 +21,15 @@ class Kernel
     private $app;
 
     /**
-     * @const string
-     */
-    const configPath = __DIR__.DIRECTORY_SEPARATOR.'..'.DIRECTORY_SEPARATOR."Resources".DIRECTORY_SEPARATOR."config".DIRECTORY_SEPARATOR."commands.yml";
-
-    /**
      * Kernel constructor.
      */
     public function __construct()
     {
-        $this->app = new Application("Syntaxerro CMD Tools", "0.0.3 alpha");
+        $this->app = new Application("Syntaxerro CMD Tools", "0.0.4 alpha");
     }
 
     /**
-     * Load commands from $ProjectRoot/src/SyntaxErro/Resources/config/commands.yml
+     * Load commands from SyntaxErro\Command namespace.
      *
      * @return Kernel
      * @throws FileNotFoundException
@@ -46,38 +39,12 @@ class Kernel
      */
     public function loadCommands()
     {
-        foreach($this->validateCommandsFile() as $command) {
+        foreach(new \DirectoryIterator(__DIR__.DIRECTORY_SEPARATOR."..".DIRECTORY_SEPARATOR."Command") as $command) {
+            if(!$command->isFile() || $command->isDot()) continue;
+            $command = 'SyntaxErro\Command\\'.str_replace('.php', '', $command->getFilename());
             $this->app->add($this->validateCommandClass($command));
         }
         return $this;
-    }
-
-    /**
-     * Throws some exceptions if not valid $ProjectRoot/src/SyntaxErro/Resources/config/commands.yml
-     * or not exist, or is not readable.
-     * Parse yaml file and return parsed array.
-     *
-     * @return array
-     * @throws FileNotFoundException
-     * @throws FileNotReadableException
-     * @throws InvalidConfigException
-     */
-    private function validateCommandsFile()
-    {
-        if(!file_exists(static::configPath)) {
-            throw new FileNotFoundException(sprintf("Not found file '%s'", static::configPath));
-        }
-
-        if(!is_readable(static::configPath)) {
-            throw new FileNotReadableException(sprintf("Cannot read '%s' file. Check permissions.", static::configPath));
-        }
-
-        $parser = new Parser();
-        $commands = $parser->parse(file_get_contents(static::configPath));
-        if( $commands === null || !is_array($commands) || !(count($commands)) ) {
-            throw new InvalidConfigException(sprintf("Not found commands in '%s' config file.", static::configPath));
-        }
-        return $commands;
     }
 
     /**
@@ -91,13 +58,13 @@ class Kernel
     {
         if(!class_exists($commandClassName)) {
             throw new InvalidCommandException(
-                sprintf("Not found '%s' class registered as command in '%s' file.", $commandClassName, static::configPath)
+                sprintf("Class '%s' is not loaded.", $commandClassName)
             );
         }
 
         if(!is_subclass_of($commandClassName, '\Symfony\Component\Console\Command\Command')) {
             throw new InvalidCommandException(
-                sprintf('Class \'%s\' registered as command in \'%s\' file is not children of \Symfony\Component\Console\Command\Command.', $commandClassName, static::configPath)
+                sprintf('Class \'%s\' registered as command must be a children of \Symfony\Component\Console\Command\Command.', $commandClassName)
             );
         }
 
